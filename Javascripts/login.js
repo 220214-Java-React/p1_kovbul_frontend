47d416bfaf6249de9e78e2987b28b2524e2f72c8
Tyler Bulger <tybulger@gmail.com>
2022-04-02 17:30:15 -0400

    minor pathway fixes

--- Javascripts/login.js
+++ Javascripts/login.js
@@ -1,18 +1,36 @@
 const BASE_API_URL = "http://localhost:8080/api";
+const RESOURCE_URL = `${BASE_API_URL}/login`;
 
-function getUsers() {
-  fetch(`${BASE_API_URL}/users`)
-    .then((response) => response.json())
-    .then((data) => createUserElements(data));
+async function loginUser(){
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-  console.log(createUserElements(data));
-}
+    const login = {
+        username: username,
+        password:password
+    }
+        if (login.username && login.password){
+        const currentUserJSON = JSON.stringify(login);
+        console.log();
+        let data = await fetch(RESOURCE_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: currentUserJSON
+            
+    
+        }).then(response => response.json())
+        .then((data)=>console.log(data))
 
-const content = document.getElementById("login");
-
-function createUserElements(userData) {
-  console.log(userData);
-  userData.forEach((user) => createUser(user));
-}
-
-console.log();
+        console.log(data);
+    }
+    
+        if(response.status == 200){
+    
+        }else{
+    
+        }
+        console.log(currentUserJSON);
+    }
+    
